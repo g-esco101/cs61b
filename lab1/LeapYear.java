90f509f809718f9fc579cdcc65eaf6db7c285018
Gov <govinda.escobar@gmail.com>
2020-04-29 17:13:27 -0700

    added isLeapYear method to LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,16 @@
  */
 public class LeapYear {
 
+    /** Determines if year is a leap year.
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
